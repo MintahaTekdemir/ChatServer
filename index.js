1b0888a5b01f92e080467e9b5e597bf274437b49
MintahaTekdemir <mintaha.tekdemir@student.reutlingen-university.de>
2019-03-25 12:10:17 +0100

    Public message with date, username fixed User register with username fixed Userlist for connected/disconnected user fixed TODO: CSS, private message,multi-media file

--- index.js
+++ index.js
@@ -1,45 +1,66 @@
+/**
+ *@author Mintaha Tekdemir
+ */
+
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
-users =[];
+var newuser = [];
 
-app.get('/', function(req, res){
+app.get('/', function(req, res) {
     res.sendFile(__dirname + '/index.html');
 });
 
-http.listen(3000, function(){
+http.listen(3000, function() {
     console.log('listening on *:3000');
 });
 
-io.on('connection', function(socket){
-    console.log('a user connected');
-    socket.on('disconnect', function(){
-        console.log('user disconnected');
-    });
-});
+// connect a user
+io.on('connection', function(socket) {
+    console.log(" a user connected");
+    // disconnect a user
+    // if the user leaves the chat room, the userlist will updated
+    //and a message is generated to the chat room with offline
+    socket.on('disconnect', function(user) {
+        console.log("user disconnected");
+        var date = new Date();
+        if (!socket.names) return;
+        newuser.splice(newuser.indexOf(socket.names), 1);
+        io.emit('usernames', newuser);
+        io.sockets.emit('chat message', {message : socket.names + " is offline", date : date
+        });
 
-io.on('connection', function(socket){
-    socket.on('added user',function(data,callback){
-        if(users.indexOf(data)!=-1){
-            callback(false);
-        }else{
+    });
+
+    // user register with a username
+    // check of username is valid
+    // if the username is valid, user has join to the chat
+    // and the user list is updated
+    //a message is generated when the new user connects to the chat room
+    socket.on('login user', function(username, callback) {
+        console.log(username);
+        var date = new Date();
+        if (newuser.indexOf(username) == -1) {
+            socket.names = username;
+            newuser.push(socket.names);
+            io.emit('usernames', newuser);
+            io.sockets.emit('chat message', {message : socket.names + " is online", date : date
+            });
             callback(true);
-            socket.users=data;
-            users.push(socket.users);
-            io.sockets.emit('newuser',users);
+        } else {
+            callback(false);
         }
     });
 
-    socket.on('chat message', function(msg){
-        console.log('message: ' + msg);
+    // the chat message is public and displayed in chronological order with a date and  username
+    socket.on('chat message', function(msg) {
+        var date = new Date();
+        io.sockets.emit('chat message', {message : msg, name : socket.names, date : date
+        });
     });
-});
 
 
-io.emit('some event', { for: 'everyone' });
 
-io.on('connection', function(socket){
-    socket.broadcast.emit('hi');
 });
 
 
